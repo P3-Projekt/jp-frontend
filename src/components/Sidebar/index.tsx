7655bf8d01e6e6f0c506275159cc2a9185eef088
Kristoffer <XA68OU@student.aau.dk>
2024-10-30 22:44:04 +0100

    Tilføjet <Link href="/"> på JP-Grønt logoet

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -3,17 +3,20 @@ import React from 'react';
 import SidebarItem from './SidebarItem';
 import sidebarConfig from '@/config/sidebar';
 import Image from 'next/image';
+import Link from "next/link";
 
 const Sidebar: React.FC = () => {
     return (
         <div className="flex justify-start w-[350px]">
             <div className="sidebar bg-sidebarcolor p-8 w-full">
-			<Image
-					src="/logo.png"
-					width={500}
-					height={500}
-					alt="Logo"
+				<Link href="/">
+					<Image
+						src="/logo.png"
+						width={500}
+						height={500}
+						alt="Logo"
 					/>
+				</Link>
                 <ul>
                     {sidebarConfig.map((item, index) => (
                         <li key={index}>
