b1e1ea4701a7ea9d92a0954b65be779a9ec0bccb
Kristoffer <XA68OU@student.aau.dk>
2024-11-14 00:06:29 +0100

    Lavet en bedre 404 side

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import Link from "next/link";
+import Image from 'next/image'
+import React from "react";
 
 const NotFound: React.FC = () => {
 	return (
-		<div style={{ textAlign: 'center', marginTop: '50px' }}>
-			<h1>404 - Not Found</h1>
-			<p>Sorry, the page you are looking for does not exist.</p>
+		<div className="flex flex-col justify-center items-center text-center min-h-screen -mt-24">
+			<div className="flex flex-row items-center justify-center">
+				<h1 className="text-9xl -mr-6" style={{ textShadow: '#2D5446 6px 2px' }}>4</h1>
+				<Image
+					src="/plant.png"
+					alt="plant"
+					width={360}
+					height={360}
+				/>
+				<h1 className="text-9xl -ml-6" style={{ textShadow: '#2D5446 6px 2px' }}>4</h1>
+			</div>
+			<div>
+				<p className="text-4xl" style={{ textShadow: 'white -1px 0.75px' }}>Denne side er i gang med at gro.</p>
+			</div>
+			<div>
+				<p className="text-2xl">Mens den bliver færdig, kan du se på mikrogrønt <Link href="/" className="underline">her</Link></p>
+			</div>
 		</div>
 	);
 };
