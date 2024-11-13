df13d512bc6edf1566b9c657fb33c80cd1eac4b8
Kristoffer <XA68OU@student.aau.dk>
2024-11-14 00:09:39 +0100

    Rettelser til 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const NotFound: React.FC = () => {
 	return (
 		<div className="flex flex-col justify-center items-center text-center min-h-screen -mt-24">
-			<div className="flex flex-row items-center justify-center">
+			<div aria-label="404" className="flex flex-row items-center justify-center">
 				<h1 className="text-9xl -mr-6" style={{ textShadow: '#2D5446 6px 2px' }}>4</h1>
 				<Image
 					src="/plant.png"
@@ -16,10 +16,10 @@ const NotFound: React.FC = () => {
 				<h1 className="text-9xl -ml-6" style={{ textShadow: '#2D5446 6px 2px' }}>4</h1>
 			</div>
 			<div>
-				<p className="text-4xl" style={{ textShadow: 'white -1px 0.75px' }}>Denne side er i gang med at gro.</p>
+				<p aria-label="Siden blev ikke fundet" className="text-4xl">Denne side er i gang med at gro.</p>
 			</div>
 			<div>
-				<p className="text-2xl">Mens den bliver færdig, kan du se på mikrogrønt <Link href="/" className="underline">her</Link></p>
+				<p className="text-2xl">Mens den bliver færdig, kan du se på mikrogrønt <Link href="/" className="underline" aria-label="Tilabe til 2D kortet">her</Link></p>
 			</div>
 		</div>
 	);
