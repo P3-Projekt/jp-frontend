f42c86f016a4b314f7967b1aff927089c412d07f
Fruttee <JP08AK@student.aau.dk>
2024-11-22 17:57:21 +0100

    Ændrede farver for at passe bedre til de andre sider

--- src/app/pregermination/page.tsx
+++ src/app/pregermination/page.tsx
@@ -5,42 +5,40 @@ import BatchReadyBox from "@/components/PregerminationMenu/BatchReadyBox/index";
 import RackBox from "@/components/Rack";
 
 const PreGerminationPage: React.FC = () => {
-    console.log('Rendering page')
-    
-    const [pregerminatingBatches, setPregerminatingBatches] = useState<any[]>([]);
+    // const [pregerminatingBatches, setPregerminatingBatches] = useState<any[]>([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('http://localhost:8080/PreGerminatingBatches');
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         try {
+    //             const response = await fetch('http://localhost:8080/PreGerminatingBatches');
 
-                if (!response.ok) {
-                    throw new Error('Fetch failed');
-                }
+    //             if (!response.ok) {
+    //                 throw new Error('Fetch failed');
+    //             }
 
-                const result = await response.json();
+    //             const result = await response.json();
 
-                console.log(result);
+    //             console.log(result);
 
-                setPregerminatingBatches(result.needsMorePreGermination);
+    //             setPregerminatingBatches(result.needsMorePreGermination);
 
-            } catch (error) {
-                alert(error);
-            }
-        }
+    //         } catch (error) {
+    //             alert(error);
+    //         }
+    //     }
 
-        fetchData();
-    }, []);
+    //     fetchData();
+    // }, []);
 
     return (
         <div className="flex">
             {/* Grey "Forspiring" background */}
-            <div className="w-[350px] fixed h-full bg-[#ababab] left-[350px]">
-                <h1 className="text-black font-bold text-2xl text-center mb-4">Forspiring</h1>
+            <div className="w-[350px] fixed h-full bg-[#d9d9d9] left-[350px]">
+                <h1 className="text-black font-bold text-2xl text-center mt-4 mb-4">Forspiring</h1>
 
                 {/* "Spire" box for all germinating batches */}
-                <div className="bg-[#606060] p-4 space-y-4 mb-4">
-                    <p className="text-white font-bold text-lg">Spirer</p>
+                <div className="bg-[#a5a4a2] p-4 space-y-4 mb-4">
+                    <p className="text-black font-bold text-lg">Spirer</p>
                     <GerminationBox 
                         plantType="Tomat"
                         amount={50}
@@ -54,8 +52,8 @@ const PreGerminationPage: React.FC = () => {
                 </div>
 
                 {/* "Klar" box for all ready batches */}
-                <div className="bg-[#606060] p-4 space-y-4">
-                    <p className="text-white font-bold text-lg">Klar</p>
+                <div className="bg-[#a5a4a2] p-4 space-y-4">
+                    <p className="text-black font-bold text-lg">Klar</p>
                     <BatchReadyBox 
                         plantType="Tomat"
                         amount={50}
@@ -68,9 +66,16 @@ const PreGerminationPage: React.FC = () => {
             </div>
 
             {/* Container for rack components */}
-            <div className="left-[800px] fixed">
+            <div className="left-[900px] top-[200px] fixed">
                 <RackBox
                     name="Reol 1"
+                    locationX={500}
+                    locationY={500}
+                />
+            </div>
+            <div className="left-[1100px] top-[200px] fixed">
+                <RackBox
+                    name="Reol 2"
                     locationX={500}
                     locationY={500}
                 />
