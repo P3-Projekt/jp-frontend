9e7d934d389260d1ba97c9d43337d05984163fb4
Jacob Højlund Simonsen <jsimon23@student.aau.dk>
2024-11-24 23:34:09 +0100

    Shelf: Use  shelf context to re render the component whenever there is an update to the context, rack: remove using context as it doesnt need the context of the shelves.

--- src/components/Rack/Shelf/index.tsx
+++ src/components/Rack/Shelf/index.tsx
@@ -1,28 +1,35 @@
-import { useShelfContext } from "@/app/pregermination/shelfcontext";
-import React from "react";
+import useShelfContext from "@/app/pregermination/context";
+import React, { useEffect, useState } from "react";
 
-interface Shelf {
-    inputVisible: boolean;
-    availableSpace: number;
-}
-
-interface ShelfProps {
-    inputVisible: boolean;
-    availableSpace: number;
+export interface ShelfProps {
     index: number;
+    rack: number;
 }
 
-const ShelfBox: React.FC<ShelfProps> = ({index, inputVisible,  availableSpace}) => {
-    const { updateShelfData } = useShelfContext();
+const ShelfBox: React.FC<ShelfProps> = ({index, rack}) => {
+    const { shelfMap } = useShelfContext();
 
-    const handleChangeAvailableSpace = (newInputVisible: boolean, newAvailableSpace: number) => {
-        updateShelfData(index, {inputVisible: newInputVisible, availableSpace: newAvailableSpace})
-    }
+    const [availableSpace, setAvailableSpace] = useState<number>(0);
+
+    useEffect(() => {
+        if (shelfMap !== undefined) {
+            const shelfArray = shelfMap.shelves.get(rack);
+            if (shelfArray !== undefined) {
+                const space = shelfArray.at(index);
+                if (space) {
+                    setAvailableSpace(space);
+                }
+            } else {
+                console.log("Setting space to 0");
+                setAvailableSpace(0);
+            }
+        }
+    }, [shelfMap, rack, index]);
 
     return (
         // Shelf container
         <div className="flex-1 flex items-center justify-center rounded-lg bg-[#a5a4a2]">
-            {inputVisible &&
+            {availableSpace > 0 &&
                 <div className="flex items-center">
                     <div className="w-6 h-4 bg-[#f3f2f0] rounded border border-[#2b4e42]"/>
                     <div className="text-black text-center ml-1">/ {availableSpace}</div>
--- src/components/Rack/index.tsx
+++ src/components/Rack/index.tsx
@@ -1,25 +1,21 @@
 import React from "react";
 import ShelfBox from "./Shelf";
-import { useShelfContext } from "@/app/pregermination/shelfcontext";
 
 interface RackProps {
-    name: string;
     id: number;
     numberOfShelves: number;
     locationX: number;
     locationY: number;
 }
 
-const RackBox: React.FC<RackProps> = ({name, id, numberOfShelves, locationX, locationY}) => {
-    const { shelves } = useShelfContext();
-
+const RackBox: React.FC<RackProps> = ({id, numberOfShelves, locationX, locationY}) => {
     return (
         // Rack container
         <div className="flex flex-col h-[300px] w-[150px] bg-[#2b4e42] rounded-lg p-1">
 
             {/* Rack name container */}
             <div className="bg-[#2b4e42] rounded-lg mb-1">
-                <p className="text-center text-white rounded-lg">{name}</p>
+                <p className="text-center text-white rounded-lg">{`Reol #${id}`}</p>
             </div>
 
             {/* Shelf container */}
@@ -27,9 +23,8 @@ const RackBox: React.FC<RackProps> = ({name, id, numberOfShelves, locationX, loc
                 {Array.from({ length: numberOfShelves }).map((_, index) => (
                     <ShelfBox
                         key={`${id}#${index}`}
-                        inputVisible={shelves[index]?.inputVisible ?? false}
-                        availableSpace={shelves[index]?.availableSpace ?? 0}
                         index={index}
+                        rack={id}
                     />
                 ))}
             </div>
