e0dc9051393779ff384795f0c72cfa5010d25af2
Fruttee <JP08AK@student.aau.dk>
2024-11-25 22:27:32 +0100

    AAUJP-109 PlaceAmountContextType og provider
    Co-authored-by: Jacob Højlund Simonsen <jsimon23@student.aau.dk>

--- src/app/pregermination/context.tsx
+++ src/app/pregermination/context.tsx
@@ -11,7 +11,7 @@ interface ShelfContextType {
     setActiveBatchId: (batchId: number | null) => void;
 }
 
-export const ShelfContext = createContext<ShelfContextType | undefined>(undefined);
+const ShelfContext = createContext<ShelfContextType | undefined>(undefined);
 
 export const ShelfProvider: React.FC<{children: ReactNode}> = ({ children }) => {
     const [shelfMap, setShelfMap] = useState<ShelfMap>({
@@ -31,9 +31,39 @@ export function useShelfContext() {
     const shelfMap = useContext(ShelfContext);
 
     if (shelfMap === undefined) {
-        throw new Error("useShelfContext must be used with a ShelfContext");
+        throw new Error("useShelfContext must be used with a ShelfContextProvider");
     }
     return shelfMap;
 }
 
-export default useShelfContext;
+// The total amount that is currently placed from a batch
+interface PlacedAmountContextType {
+    placedAmount: number;
+    setPlacedAmount: (amount: number) => void;
+    batchAmount: number | null;
+    setBatchAmount: (amount: number | null) => void;
+}
+
+const PlacedAmountContext = createContext<PlacedAmountContextType | undefined>(undefined);
+
+export const PlacedAmountProvider: React.FC<{children: ReactNode}> = ({ children }) => {
+    const [placedAmount, setPlacedAmount] = useState(0);
+    const [batchAmount, setBatchAmount] = useState<number | null>(null);
+
+    return (
+        <PlacedAmountContext.Provider value={{ placedAmount, setPlacedAmount, batchAmount, setBatchAmount }}>
+            {children}
+        </PlacedAmountContext.Provider>
+
+    )
+}
+
+export function usePlacedAmountContext() {
+    const placedAmount = useContext(PlacedAmountContext);
+
+    if (placedAmount === undefined) {
+        throw new Error("usePlacedAmountContext must be used with a PlacedAmountContextProvider");
+    }
+
+    return placedAmount;
+}
