aec8d3b70632258598b4e498d33aab907622e729
Fruttee <JP08AK@student.aau.dk>
2024-11-26 16:25:24 +0100

    AAUJP-109 Rettede light- og darkgrey.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,8 +16,8 @@ const config: Config = {
 				textcolor: "#3A3C47",
 				sidebarcolor: "#F4F3F0",
 				subbordercolor: "#E6E3DC",
-				lightgrey: "#A5A5A5",
-				darkgrey: "#D9D9D9"
+				lightgrey: "#D9D9D9",
+				darkgrey: "#A5A5A5"
 			},
 		},
 	},
