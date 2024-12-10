a8b2fe893c4dfcc99d3cdfd6577d92b7e8a2b23f
Victor Hjelmberg Feddersen <69452489+victorhjelmberg@users.noreply.github.com>
2024-12-10 21:59:29 +0100

    Centralised authentication

--- src/components/wrappers/MainSiteWrapper.tsx
+++ src/components/wrappers/MainSiteWrapper.tsx
@@ -1,8 +1,6 @@
 "use client";
 import { ReactNode } from "react";
 import { cn } from "@/app/lib/utils";
-import { useEffect } from "react";
-import { useRouter, usePathname } from "next/navigation";
 
 const MainSiteWrapper = ({
 	className,
@@ -11,17 +9,7 @@ const MainSiteWrapper = ({
 	className?: string;
 	children: ReactNode;
 }) => {
-	const router = useRouter();
-	const path = usePathname();
 
-	useEffect(() => {
-		const authToken = localStorage.getItem("authToken");
-		if (!authToken && path !== "/login") {
-			router.push("/login");
-		} else if (authToken && path === "/login") {
-			router.push("/");
-		}
-	}, [router, path]);
 	return (
 		<div
 			className={cn(
